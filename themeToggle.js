ea2c662b93ab35124951c15a3eae750bff8a9771
JustAnotherDoge <128501862+JustAnotherDoge@users.noreply.github.com>
2024-12-02 12:57:25 +0300

    Add files via upload

--- themeToggle.js
+++ themeToggle.js
@@ -8,13 +8,16 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('themeChangerButton').addEventListener('click', function() {
         console.log('click');
         if (document.documentElement.classList == '') {
-            document.documentElement.className = 'lightT'
+            document.documentElement.className = 'lightT';
+            document.getElementById('TCpic').src = "pics/Light.png";
         }
         else if (document.documentElement.classList == 'lightT') {
-            document.documentElement.className = 'darkT'
+            document.documentElement.className = 'darkT';
+            document.getElementById('TCpic').src = "pics/Dark.png";
         }
         else {
-            document.documentElement.className = ''
+            document.documentElement.className = '';
+            document.getElementById('TCpic').src = "pics/DN.png";
         }
     });
 });
